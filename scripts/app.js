97a8e30cbd804640b41ea0f1d8d884946bbdf0e6
Adilson Caculo <adilsoncaculosuares@gmail.com>
2021-08-21 01:57:24 +0100

    tride

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,48 @@
 //menu toglle
-const on = document.querySelector('.add')
-const img = document.querySelector('.img')
-const remove = 
-img.addEventListener('click', click)
-function click(){
-    
-     on.classList.add('on')
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
 
-     return true
-    
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+ 
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
 }
-if(click == true){
-       
-    img.addEventListener('click',()=>{
-    on.classList.remove('on')
-})}
 
-
-console.log(on)
-
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
 
 
 //Slide 
